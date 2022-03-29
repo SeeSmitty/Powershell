58f405adbc1e11e86477475af6e9c996f05957dd
SeeSmitty <102568084+SeeSmitty@users.noreply.github.com>
2022-03-28 22:39:15 -0400

    Create Add-UsersToAzureADGroup.ps1
    Script to use CSV import for bulk adding users to an Azure AD Group

---
+++ Add-UsersToAzureADGroup.ps1
@@ -0,0 +1,27 @@
+
+#connect to azure ad
+Connect-AzureAD
+
+#import a CSv with the list of users to be added to the group
+$yammer = Import-Csv "C:\Users\SeeSmitty\Downloads\UserList.csv"
+#Name of the group being added
+$group = "All Employees"
+
+
+#get the object ID from Azure
+$GroupObjectID = Get-AzureADGroup -SearchString $group | Select -Property ObjectID
+
+
+#roll through the list to look up each user and add to the group. 
+foreach ($y in $yammer){
+    $y2 = Get-AzureADUser -ObjectId $y.userPrincipalName | Select -Property ObjectID
+    try {
+        Add-AzureADGroupMember -ObjectId $GroupObjectID.ObjectID -RefObjectId $y2.ObjectId -InformationAction SilentlyContinue
+    }
+    catch {
+        Write-Host $y.userPrincipalName "is already a member"
+    }
+}
+
+#Disconnect Azure AD
+Disconnect-AzureAD
